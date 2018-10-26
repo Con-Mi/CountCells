a1053c49d3eba77ee099a0b9df3af6e2b51d5cc6
Con-MI <aspris.marios@gmail.com>
2018-10-26 11:21:57 +0200

    Updates

---
+++ modules/make_data_test.py
@@ -0,0 +1,31 @@
+import os
+import skimage
+import sys
+from tqdm import tqdm
+import numpy as np
+from PIL import Image
+
+
+TRAIN_PATH = "../data/DSB-Stage1-Test/"
+
+train_ids = next(os.walk(TRAIN_PATH))[1]
+traind_ids = sorted(train_ids)
+print("Getting images and reconstructing masks..")
+sys.stdout.flush()
+
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    path = TRAIN_PATH+id_
+    img = skimage.io.imread(path + "/images/" + id_ + ".png")
+    #skimage.io.imsave("../data/GenData/TrainData/images/" + str(n) + "_.png", img)
+    im = Image.fromarray(img)
+    #im.save("../data/GenData/TrainData/images/" + str("%04d" % n) + "_.png")
+    im.save("../data/AugmData/images/" + str("%04d" % n) + "_.png")
+    
+    mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.bool)
+    for mask_file in next(os.walk(path+"/masks/"))[2]:
+        mask_ = skimage.io.imread(path + "/masks/" + mask_file)
+        mask = np.maximum(mask, mask_)
+    #skimage.io.imsave("../data/GenData/TrainData/labels/" + str(n) + "_.png", mask)
+    mask_im = Image.fromarray(mask)
+    mask_im.save("../data/AugmData/labels/" + str("%04d" % n) + "_.png")
+    
---
+++ modules/make_green.py
@@ -0,0 +1,31 @@
+import os
+import skimage
+from skimage import color
+import sys
+from tqdm import tqdm
+import numpy as np
+from PIL import Image
+
+TRAIN_PATH = "../data/DSB-Stage1/"
+
+train_ids = next(os.walk(TRAIN_PATH))[1]
+traind_ids = sorted(train_ids)
+print("Getting and reconstructing images and masks..")
+sys.stdout.flush()
+green = [0, 10, 0]
+
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    path = TRAIN_PATH+id_
+    img = skimage.io.imread(path + "/images/" + id_ + ".png")
+    img = color.rgba2rgb(img)
+    img_g = green*img
+    img_g = (img_g*255).astype(np.uint8)
+    im = Image.fromarray(img_g)
+    im.save("../data/GenData/TrainData/images/" + str("%04d" % n) + "_.png")
+
+    mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.bool)
+    for mask_file in next(os.walk(path+"/masks/"))[2]:
+        mask_ = skimage.io.imread(path + "/masks/" + mask_file)
+        mask = np.maximum(mask, mask_)
+    mask_im = Image.fromarray(mask)
+    mask_im.save("../data/GenData/TrainData/labels/" + str("%04d" % n) + "_.png")
